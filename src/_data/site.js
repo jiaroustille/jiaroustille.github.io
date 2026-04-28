4347c79e67fcd54d66ee4b91fa0dc654e9d3e68f
Jia-Rou <jiaroustille@proton.me>
2026-04-28 20:28:54 +0200

    add site data for more defaults

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,13 +1,20 @@
 export default {
   title: "My Site",
+  description: "A personal site with art showcases, reflections and eventual blog entries.",
+  url: "https://jiaroustille.github.io",
   domain: "jiaroustille.github.io",
+  favicon: "/img/favicon.png",
+  image: "/img/image.png",
+
   author: {
     name: "Jia-Rou",
+    url: "https://jiaroustille.github.io",
     relme: [
       { key: "https://github.com/jiaroustille", label: "GitHub" },
-      { key: "https://bsky.social.com/name", label: "BlueSky" }
+      { key: "https://bsky.app/profile/jiaroustille.bsky.social", label: "Bluesky" }
     ]
   },
+
   feed: { rss: "/rss.xml" },
   repo: "my-site"
 }
