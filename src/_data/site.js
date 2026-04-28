327e74ad5298d14702e4b3e8340c932a475ca3ac
Jia-Rou <jiaroustille@proton.me>
2026-04-28 19:21:39 +0200

    add site metadata and git log data

---
+++ src/_data/site.js
@@ -0,0 +1,13 @@
+export default {
+  title: "My Site",
+  domain: "jiaroustille.github.io",
+  author: {
+    name: "Jia-Rou",
+    relme: [
+      { key: "https://github.com/jiaroustille", label: "GitHub" },
+      { key: "https://bsky.social.com/name", label: "BlueSky" }
+    ]
+  },
+  feed: { rss: "/rss.xml" },
+  repo: "my-site"
+}
