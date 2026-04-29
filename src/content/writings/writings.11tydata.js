09587421303d8b06551b4ccfafa186c4b9b3e8d2
Jia-Rou <jiaroustille@proton.me>
2026-04-29 20:43:24 +0200

    add optional manual title

--- src/content/writings/writings.11tydata.js
+++ src/content/writings/writings.11tydata.js
@@ -2,7 +2,7 @@ import capitalize from "../../_11ty/plugins/capitalizeFilter.js"
 
 export default {
   eleventyComputed: {
-    title: (data) => capitalize(data.page.fileSlug),
+    title: (data) => data.title || capitalize(data.page.fileSlug),
     permalink: (data) => `/writings/${data.page.fileSlug}/`,
     layout: "article.html"
   }
