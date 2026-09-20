eb9918a6d15f16c2cd124a0072eded6c099983f0
Jia-Rou <jiaroustille@proton.me>
2026-09-20 21:55:45 +0200

    change redirectUri

--- src/_data/apiKeys.js
+++ src/_data/apiKeys.js
@@ -1,6 +1,7 @@
 export default {
   spotify: {
     clientId: "4b074dd66f75468d8292f1e6fee2f071",
-    redirectUri: "http://127.0.0.1:8080/dashboard"
+    redirectUri: "https://jiaroustille.github.io/dashboard"
+    // redirectUri: "http://127.0.0.1:8080/dashboard"
   }
 }
